b68ba39f82e91b3b7b0f33765eca2813b675097c
cgfaraone <christiangenn99@gmail.com>
2025-11-08 20:39:50 +0100

    fix: Update contact email across all files
    - Replace the outdated contact email address from 'christiangenn99+promptrca@gmail.com' to 'info@promptrca.com' in all relevant source files to ensure accurate communication.

--- src/promptrca/tools/apigateway_tools.py
+++ src/promptrca/tools/apigateway_tools.py
@@ -16,7 +16,7 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-Contact: christiangenn99+promptrca@gmail.com
+Contact: info@promptrca.com
 
 """
 
